8823d944204d97d9b2f058104b97c06c8570638c
zhifeng ou <zhifengou@hotmail.com>
2018-09-26 14:30:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,25 @@
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+ 
   def self.all
     @@all
   end
-
+ 
   def save
-    self.class.all << self
+    @@all << self
   end
-
+ 
+ def self.create
+    song = self.new
+    song.save
+    song
+  end
+def self.new_by_name(name)
+  song=self.new
+  song.name=name
+  song.save
+  song
+end
 end
